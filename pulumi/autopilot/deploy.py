b10a416e638e411d4514deb0b2a03171d10e3f9b
adekoyadapo <49321398+adekoyadapo@users.noreply.github.com>
2022-07-08 13:48:46 -0600

    update pulumi

--- pulumi/autopilot/deploy.py
+++
@@ -1,9 +0,0 @@
-import pulumi
-import pulumi_gcp as gcp
-
-default = gcp.service_account.Account("default",
-    account_id="service-account-id",
-    display_name="Service Account")
-primary = gcp.container.Cluster("primary",
-    enable_autopilot=True,
-    location="us-central1-a")
